ec9dfc30d03014e015f46d8067e4fb232b4c5d77
cobaltgit <criterion@chitco.co.uk>
2021-12-18 19:23:43 +0000

    Code formatting

--- cogs/commands/utils.py
+++ cogs/commands/utils.py
@@ -1,22 +1,24 @@
-import discord
-import os, sys
+import os
+import sys
 from datetime import datetime
+
 from discord.ext import commands
 
 
 class Utilities(commands.Cog):
     """General utilities"""
+
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(name="restart", brief="Restart the bot", description="Restart the bot with exec [ OWNER ONLY ]")
     @commands.is_owner()
-    async def restart(self,ctx):
+    async def restart(self, ctx):
         await ctx.send("Restarting bot...")
         print(f"[{datetime.now().strftime('%d-%m-%Y %H:%M:%S')}] Restarting bot")
         await self.bot.close()
         os.execv(sys.executable, ["python"] + sys.argv)
-    
+
 
 def setup(bot):
     bot.add_cog(Utilities(bot))
