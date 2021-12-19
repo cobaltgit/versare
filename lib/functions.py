e8b33c1705018f1e3c548314f8693c8765797bcb
cobaltgit <criterion@chitco.co.uk>
2021-12-19 19:48:51 +0000

    Obtain log channel from guild instead of context

--- lib/functions.py
+++ lib/functions.py
@@ -1,11 +1,11 @@
+import discord
 from asqlite import connect
-from discord.ext import commands
 
 
-async def get_log_channel(ctx: commands.Context) -> int:
+async def get_log_channel(guild: discord.Guild) -> int:
     async with connect("db/guild.db") as connection:
         async with connection.cursor() as cur:
-            await cur.execute("SELECT log_channel_id FROM logging WHERE guild_id = ?", (ctx.guild.id,))
+            await cur.execute("SELECT log_channel_id FROM logging WHERE guild_id = ?", (guild.id,))
             result = await cur.fetchone()
             if result:
                 return result[0]
