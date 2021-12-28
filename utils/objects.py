d9a5023d246db68cfbbca6e855fa13772e457793
cobaltgit <criterion@chitco.co.uk>
2021-12-28 19:15:43 +0000

    Use discord time instead of system time

--- utils/objects.py
+++ utils/objects.py
@@ -1,9 +1,7 @@
-from datetime import datetime
-
 import discord
 
 
 class BaseEmbed(discord.Embed):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.timestamp = datetime.utcnow()
+        self.timestamp = discord.utils.utcnow()
