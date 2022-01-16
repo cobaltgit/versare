214c90942a3126d4a7d26ab1d083baeb61017683
cobaltgit <criterion@chitco.co.uk>
2022-01-16 13:16:07 +0000

    Add timeout command

--- cogs/commands/mod.py
+++ cogs/commands/mod.py
@@ -1,7 +1,12 @@
 from __future__ import annotations
-from discord.ext import commands
+
+from contextlib import suppress
+from typing import Optional
+
 import discord
-from utils.objects import BaseEmbed
+from discord.ext import commands
+
+from utils.objects import BaseEmbed, TimeConverter
 
 
 class Moderation(commands.Cog):
@@ -17,6 +22,7 @@ class Moderation(commands.Cog):
         invoke_without_command=True,
     )
     async def snipe(self, ctx: commands.Context) -> discord.Message:
+        await ctx.defer()
         target = await self.bot.db.fetchrow("SELECT * FROM sniper WHERE channel_id = $1", ctx.message.channel.id)
         if not target:
             return await ctx.send(":envelope: No message to snipe")
@@ -39,6 +45,7 @@ class Moderation(commands.Cog):
         description="Opt out of having your deleted messages sniped by other users",
     )
     async def optout(self, ctx: commands.Context) -> discord.Message:
+        await ctx.defer()
         user_optout = await self.bot.db.fetchrow("SELECT * FROM snipe_optout WHERE guild_id = $1", ctx.guild.id)
 
         if user_optout and ctx.author.id in user_optout:
@@ -55,6 +62,7 @@ class Moderation(commands.Cog):
         description="Opt back in to having your deleted messages sniped by other users",
     )
     async def optin(self, ctx: commands.Context) -> discord.Message:
+        await ctx.defer()
         user_optout = await self.bot.db.fetchrow("SELECT * FROM snipe_optout WHERE guild_id = $1", ctx.guild.id)
         if ctx.author.id not in user_optout:
             return await ctx.send(":envelope: You are already opted in to being sniped")
@@ -71,6 +79,7 @@ class Moderation(commands.Cog):
         description="Get the previous contents of the last edited message in the channel - some users may opt out, which the sniper will ignore",
     )
     async def editsnipe(self, ctx: commands.Context) -> discord.Message:
+        await ctx.defer()
         target = await self.bot.db.fetchrow("SELECT * FROM editsniper WHERE channel_id = $1", ctx.message.channel.id)
         if not target:
             return await ctx.send(":envelope: No message to editsnipe")
@@ -88,6 +97,40 @@ class Moderation(commands.Cog):
         for name, value, inline in fields:
             embed.add_field(name=name, value=value, inline=inline)
         return await ctx.send(embed=embed)
+
+    @commands.command(
+        name="timeout",
+        aliases=["mute", "stfu", "silence"],
+        brief="Time out members from the server",
+        description="Time out members from the server for any duration between 1 second and 28 days",
+    )
+    @commands.has_permissions(moderate_members=True)
+    async def timeout(
+        self,
+        ctx: commands.Context,
+        member: discord.Member = commands.Option(description="Who to time out?"),
+        duration: Optional[str] = commands.Option(description="How long will the mute last?", default="10m"),
+        reason: Optional[str] = commands.Option(description="Why are you muting them?", default="No reason provided"),
+    ) -> discord.Message:
+        await ctx.defer()
+        try:
+            _duration = TimeConverter(duration)
+        except ValueError:
+            return await ctx.send(":stopwatch: Invalid duration")
+        if not 1 < _duration.seconds < 2419200:
+            return await ctx.send(f":stopwatch: Duration must be between 1 second and 28 days, got {_duration}")
+        if member.timed_out:
+            return await ctx.send(
+                f":stopwatch: Member {member} is already timed out for {TimeConverter(member.timeout_until.timestamp()-ctx.message.created_at.timestamp())}"
+            )
+
+        try:
+            await member.edit(timeout_until=discord.utils.utcnow() + _duration.delta)
+        except discord.errors.Forbidden:
+            return await ctx.send(f":stopwatch: You do not have permission to mute {member}")
+        with suppress(discord.errors.Forbidden):
+            await member.send(f"You have been muted in {ctx.guild} for {_duration}\nReason: {reason}")
+        return await ctx.send(f":stopwatch: Timed out member {member.mention} for {_duration}")
 
 
 def setup(bot: commands.Bot) -> None:
