936f69bf7b56012a73672ae18725b2d519cfd65a
cobaltgit <criterion@chitco.co.uk>
2022-01-16 19:40:12 +0000

    Bump release

--- bot.py
+++ bot.py
@@ -37,7 +37,7 @@ class Versare(commands.AutoShardedBot):
 
     def __init__(self) -> None:
 
-        self.__version__ = "0.3-rw"
+        self.__version__ = "0.3.2-rw"
 
         with open("config.yml", "r") as config_file:
             self.config = yaml.safe_load(config_file)
